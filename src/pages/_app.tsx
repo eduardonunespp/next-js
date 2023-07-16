5d6ace2bdec56ddc5521f23de991542069e64bfb
eduardonunespp <carlospn200406@gmail.com>
2023-07-16 19:46:55 -0300

    feat: add components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { LogoImg } from "../assets/logo";
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from '../styles/pages'
+import Image from 'next/image'
+
+console.log(logoImg);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+
+      <Header>
+    
+        <Image src={logoImg.src} alt="imgLogo" width={100} height={100} />
+
+      </Header>
+
+      <Component {...pageProps} />
+
+    </Container>
+    
+  );
 }
