95587d5581a2c2564fc9ab1a4efdcb8e4d33a80d
eduardonunespp <carlospn200406@gmail.com>
2023-07-16 20:20:20 -0300

    feat: add carrosel in homeContainer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import * as S from "../styles/pages/";
 import Image from "next/image";
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 import camiseta1 from "../assets/MDB8YWNjdF8xTWxtSjRHdWRoOE9qNGJafGZsX3Rlc3RfajMyTE5CczBFZFAzVWN0MmFuc29aZ3B000lvwPxZWz.png";
 import camiseta2 from "../assets/MDB8YWNjdF8xTWxtSjRHdWRoOE9qNGJafGZsX3Rlc3RfajMyTE5CczBFZFAzVWN0MmFuc29aZ3B000lvwPxZWz.png";
@@ -11,12 +13,22 @@ import camiseta3 from "../assets/MDB8YWNjdF8xTWxtSjRHdWRoOE9qNGJafGZsX3Rlc3RfajM
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
+
   return (
     <>
       <h1>Home</h1>
 
-      <S.HomeContainer>
-        <S.Product>
+      <S.HomeContainer ref={sliderRef} className="keen-slider ">
+        <S.Product className="keen-slider__slide">
           <Image src={camiseta1} alt="sim" width={520} height={480} />
 
           <S.Footer>
@@ -24,7 +36,24 @@ export default function Home() {
             <span>R$ 79,98</span>
           </S.Footer>
         </S.Product>
-        <S.Product>
+        <S.Product className="keen-slider__slide">
+          <Image src={camiseta1} alt="sim" width={520} height={480} />
+
+          <S.Footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,98</span>
+          </S.Footer>
+        </S.Product>
+        <S.Product className="keen-slider__slide">
+          <Image src={camiseta1} alt="sim" width={520} height={480} />
+
+          <S.Footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,98</span>
+          </S.Footer>
+        </S.Product>
+
+        <S.Product className="keen-slider__slide">
           <Image src={camiseta1} alt="sim" width={520} height={480} />
 
           <S.Footer>
