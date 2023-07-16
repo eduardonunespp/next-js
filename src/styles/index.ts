30ef636ad2bb52d52f21822c546b9e034b7f1585
eduardonunespp <carlospn200406@gmail.com>
2023-07-16 19:47:53 -0300

    style: add

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -15,6 +15,22 @@ export const {
   theme: {
     colors: {
       rocketSeat: "#8257e6",
+
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e'
     },
+
+    fontSizes: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
+      
+    }
   },
 });
