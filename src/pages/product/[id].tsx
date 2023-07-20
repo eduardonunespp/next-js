d794a75cc9f78377ded7f6053699335eb62b516f
eduardonunespp <carlospn200406@gmail.com>
2023-07-20 10:52:01 -0300

    feat: add Static Path and Static Props

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,30 +1,78 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { stringify } from "querystring";
-import * as S from '../../styles/pages'
-import { } from '../'
+import * as S from "../../styles/pages";
+import {} from "../";
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from "next";
+import { stripe } from "@/src/lib/stripe";
+import Stripe from "stripe";
+import Image from "next/image";
 
-const Product: React.FC = () => {
+import { ProductProps } from "../../types";
+
+const Product: React.FC<ProductProps> = ({ product }) => {
   const { query } = useRouter();
+
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    <p>...loading</p>;
+  }
 
   return (
     <S.ProductContainer>
-        <S.ImageContainer>
-          
-        </S.ImageContainer>
-        <S.ProductDetails>
-            <h1>Camiseta X</h1>
-            <span>R$ 79,90</span>
+      <S.ImageContainer>
+        <Image
+          src={product.imageUrl}
+          alt="Imagem do Produto"
+          width={520}
+          height={480}
+        />
+      </S.ImageContainer>
+      <S.ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis cumque dignissimos eveniet fuga blanditiis repellat enim repellendus repudiandae quia minus perferendis expedita accusamus id mollitia suscipit nisi accusantium, natus excepturi.</p>
-        
-            <button>
-                Comprar agora
-            </button>
-        
-        </S.ProductDetails>
+        <p>{product.description}</p>
+
+        <button>Comprar agora</button>
+      </S.ProductDetails>
     </S.ProductContainer>
   );
 };
 
 export default Product;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "prod_OHQ7vmpd5kyNSy" } }],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params!.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(price.unit_amount !== null ? price.unit_amount / 100 : 0),
+        description: product.description,
+      },
+    },
+  };
+};
