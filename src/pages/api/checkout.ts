559f07b891c15cb2f9d4d9c2de9c19aba20f495c
eduardonunespp <carlospn200406@gmail.com>
2023-07-23 09:44:36 -0300

    refactor: rename variables

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,12 +5,12 @@ import { stripe } from "../../lib/stripe";
 export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const priceId = "price_1NUrJiCLa4esDAgQyk4oW4al";
 
-  const success_url = `${process.env.NEXT_URL}/success`;
-  const cancel_url = `${process.env.NEXT_URL}/`;
+  const successUrl = `${process.env.NEXT_URL}/success`;
+  const cancelUrl = `${process.env.NEXT_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
-    success_url: success_url,
-    cancel_url: cancel_url,
+    success_url: successUrl,
+    cancel_url: cancelUrl,
     mode: "payment",
 
     line_items: [
